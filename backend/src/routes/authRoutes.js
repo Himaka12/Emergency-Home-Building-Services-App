8156acec4cb8a63b01f8316d3388663abeceb2c2
Himaka Uthpala <himakauthpala14@gmail.com>
2026-05-20 00:21:44 +0530

    update auth routes .js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const { getMe, login, register } = require('../controllers/authController');
+const { getMe, login, register, updateMe, updateProfileImage } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
+const upload = require('../middleware/uploadMiddleware');
 const validateRequest = require('../middleware/validateRequest');
-const { loginValidation, registerValidation } = require('../validations/authValidation');
+const { loginValidation, registerValidation, updateMeValidation } = require('../validations/authValidation');
 
 const router = express.Router();
 
 router.post('/register', registerValidation, validateRequest, register);
 router.post('/login', loginValidation, validateRequest, login);
 router.get('/me', protect, getMe);
+router.patch('/me', protect, updateMeValidation, validateRequest, updateMe);
+router.patch('/me/profile-image', protect, upload.single('profileImage'), updateProfileImage);
 
 module.exports = router;
