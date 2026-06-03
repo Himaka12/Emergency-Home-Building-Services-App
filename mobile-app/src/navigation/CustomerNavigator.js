c73599f2294fb8f97d361efaaaa3f2228625c6cd
Himaka Uthpala <himakauthpala14@gmail.com>
2026-06-03 20:02:42 +0530

    Navigator update

--- mobile-app/src/navigation/CustomerNavigator.js
+++ mobile-app/src/navigation/CustomerNavigator.js
@@ -1,6 +1,8 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BookingDetailsScreen from '../screens/customer/BookingDetailsScreen';
 import CreateBookingScreen from '../screens/customer/CreateBookingScreen';
+import CustomerProfileScreen from '../screens/customer/CustomerProfileScreen';
+import ManageProfileScreen from '../screens/customer/ManageProfileScreen';
 import MyBookingsScreen from '../screens/customer/MyBookingsScreen';
 import RateWorkerScreen from '../screens/customer/RateWorkerScreen';
 import ServiceCategoriesScreen from '../screens/customer/ServiceCategoriesScreen';
@@ -9,8 +11,26 @@ const Stack = createNativeStackNavigator();
 
 const CustomerNavigator = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="ServiceCategories" component={ServiceCategoriesScreen} />
+    <Stack.Navigator
+      screenOptions={{
+        contentStyle: { backgroundColor: '#f6f8fb' }
+      }}
+    >
+      <Stack.Screen
+        name="ServiceCategories"
+        component={ServiceCategoriesScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="CustomerProfile"
+        component={CustomerProfileScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="ManageProfile"
+        component={ManageProfileScreen}
+        options={{ headerShown: false }}
+      />
       <Stack.Screen name="CreateBooking" component={CreateBookingScreen} />
       <Stack.Screen name="MyBookings" component={MyBookingsScreen} />
       <Stack.Screen name="BookingDetails" component={BookingDetailsScreen} />
